5a350d7fd04ff26ec7f495b5c595abcc4ec5daa4
Lukas Benner <lukas.benner@mitutoyo-ctl.de>
2023-06-29 22:22:47 +0200

    change nuget config to enable normal installation

--- build.cmd
+++ build.cmd
@@ -12,4 +12,7 @@ cmake -E chdir "build" cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug
 cmake --build "build" --config Debug --parallel 4
 
 @rem Build nuget
-nuget pack .\gbenchmark.nuspec -OutputDirectory nuget
+nuget pack .\googlebenchmark.nuspec -OutputDirectory nuget
+
+
+@rem nuget push .\nuget\googlebenchmark.1.8.0.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey
